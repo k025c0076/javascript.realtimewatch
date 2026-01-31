b7a273a85cfd47c50be133cf50fc8585d28353b3
k025c0076 <k025c0076@g.neec.ac.jp>
2026-01-31 21:59:42 +0900

    commit

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
+// 1. 時計を動かす関数
 function updateClock() {
-    const now = new Date(); 
-
+    const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
 
-    const clockElement = document.getElementById('clock');
-    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
 }
 
+// 1秒ごとにupdateClock関数を実行する
 setInterval(updateClock, 1000);
+updateClock(); 
 
-updateClock();
-
+// 2. 背景色を変える機能
 const button = document.getElementById('changeColorBtn');
 
-button.addEventListener('click', function() {
-
-    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+button.addEventListener('click', () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
     
-    document.body.style.backgroundColor = randomColor;
+    document.body.style.backgroundColor = color;
 });
